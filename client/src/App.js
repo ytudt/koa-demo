37f485a5812bc19f663aa1e0cc5fe8bca00d6b82
dongtongtong <dongtongtong@meituan.com>
2018-05-13 20:24:10 +0800

    feat:增加地图

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,6 @@ class App extends Component {
     setTimeout(() => window.toggleLoad(false));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
