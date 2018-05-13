44ae5b29a2784b0c2725b5265196af3f171eb302
dongtongtong <dongtongtong@meituan.com>
2018-05-13 19:42:34 +0800

    fix:增加loading动画

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    setTimeout(() => window.toggleLoad(false));
     return (
       <div className="App">
         <header className="App-header">
