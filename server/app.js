a23c5ca92b8524a0eb62b866f0ff7a8454bc6325
dongtongtong <dongtongtong@meituan.com>
2018-06-25 19:56:20 +0800

    fix

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 
 const path = require('path');
 const Koa = require('koa');
-const compression = require('compression')
+// const compression = require('compression')
 const app = new Koa();
 const staticServer = require('koa-static');
 
 
-app.use(compression());
+// app.use(compression());
 app.use(staticServer(path.join(__dirname,'public')));
 
 app.listen(8080)
