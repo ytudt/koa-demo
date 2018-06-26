95611eb95792d3e92767ef52f593898fb772c627
dongtongtong <dongtongtong@meituan.com>
2018-06-26 17:37:00 +0800

    fix

--- script/publish-map.sh
+++ script/publish-map.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/ytudt/ytudt.github.io.git
+cd ytudt.github.io
+git pull
+cd ../
 # git clone git@github.com:ytudt/ytudt.github.io.git
 rm -rf server/public
 cp -R ytudt.github.io/ server/public
